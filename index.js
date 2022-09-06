19834b378f7eb80afdbdea181bafc7dbef70188c
Asumarin <asumarin18@gmail.com>
2022-09-07 01:18:52 +0300

    multiply types

--- index.js
+++ index.js
@@ -6,3 +6,9 @@ let num = 5;
 console.log(str + bool);
 console.log(str + num);
 console.log(num + bool);
+
+//Выполнить умножение различных типов(string * boolean, string * number, number * boolean)
+
+console.log(str * bool);
+console.log(str * num);
+console.log(num * bool);
