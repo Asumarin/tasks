68c5211adc57c44aca3f7015db60c322aca6fb73
Asumarin <asumarin18@gmail.com>
2022-09-07 01:32:34 +0300

    task done

--- index.js
+++ index.js
@@ -12,3 +12,15 @@ console.log(num + bool);
 console.log(str * bool);
 console.log(str * num);
 console.log(num * bool);
+
+//Выполнить деление различных типов(string/boolean, string/number, number/Boolean)
+
+console.log(str / bool);
+console.log(str / num);
+console.log(num / bool);
+
+
+//Выполнить явное преобразование(number, string, boolean)
+console.log(String(bool))
+console.log(Boolean(num))
+console.log(Number(str))
