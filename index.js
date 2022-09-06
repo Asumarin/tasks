a9d0895d098f713242aea735ad80a775c293f649
Asumarin <asumarin18@gmail.com>
2022-09-07 01:17:24 +0300

    sum types

---
+++ index.js
@@ -0,0 +1,8 @@
+//Выполнить сложение различных типов(string+boolean, string+number, number+boolean)
+let str = 'kek';
+let bool = true;
+let num = 5;
+
+console.log(str + bool);
+console.log(str + num);
+console.log(num + bool);
