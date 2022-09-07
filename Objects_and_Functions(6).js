a68d031eabb8cb767904b0174261557553d15949
Asumarin <asumarin18@gmail.com>
2022-09-07 16:17:54 +0300

    completed task 6

---
+++ Objects_and_Functions(6).js
@@ -0,0 +1,9 @@
+// Напишите функцию вычисления типа аргумента и вывод типа в консоль
+
+function learnTypeof(arg){
+    const type = typeof arg;
+    console.log(type);
+}
+
+learnTypeof(null);
+learnTypeof(true);
